93cf4a5ac0c82bcb56289c45672f3a475991643b
Parth099 <parthnpatel715@gmail.cm>
2021-06-15 19:50:18 -0400

    added shading mode, fix cell-overwriting bugs

--- js/src.js
+++ js/src.js
@@ -3,10 +3,12 @@ var numBox = 16;
 var erasor = 0;
 var rainbow = 0;
 var drawMode = 0;
+var grayMode = 0;
 var colorMain = "#000";
 
+
 function getColor(){
-    return (rainbow) ? "#"+Math.floor(Math.random()*16777215).toString(16): colorMain;
+    return (rainbow) ? '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0') : colorMain;
 }
 function getBoxSz(flag = 1){
     if(flag = 1){
@@ -14,63 +16,81 @@ function getBoxSz(flag = 1){
     }
     while(numBox < 0 || numBox > 100){
         numBox = prompt("Enter a box size, MAX: 100", 20);
-        console.log(numBox)
     }
     numBox = numBox ?? 20;
 }
 
 function DynamicPageCalc(numBox){
     let root = document.documentElement;
-    root.style.setProperty("--sz", `${boxWdth}px`)
-    root.style.setProperty("--bks", `${numBox}`)
-    root.style.setProperty("--bksz", `${boxWdth / numBox}px`)
+    root.style.setProperty("--sz", `${boxWdth}px`);
+    root.style.setProperty("--bks", `${numBox}`);
+    root.style.setProperty("--bksz", `${boxWdth / numBox}px`);
 }
 
 function pushBoxes(main){
-    let box = document.createElement("div")
-    box.classList.toggle("grid-ele")
+    let box = document.createElement("div");
+    box.classList.toggle("grid-ele");
 
     main = document.querySelector(".grid-basis");
-    DynamicPageCalc(numBox)
+    DynamicPageCalc(numBox);
 
     let clone;
     for(let i = 0; i < numBox * numBox; i++){
-        clone = box.cloneNode(true)
+        clone = box.cloneNode(true);
         clone.style['background-color'] = 'white';
         clone.setAttribute("draggable", "false");
+        clone.setAttribute("data-grayscale", "0");
         //listener
 
         clone.addEventListener("mousedown", function(e){
             drawMode ^= 1;
-            console.log("Engaged", drawMode);
+            pushColor(e);
         });
 
-        clone.addEventListener('mouseover', function(e){
-            if (!(e.target.classList.contains("colored")) && drawMode && !erasor){
-                console.log("hit")
-                e.target.style['background-color'] = getColor()
-                e.target.classList.add("colored");
-            }
-            else if(erasor && drawMode){
-                console.log("Erasing");
-                e.target.style['background-color'] = "#fff"
-                e.target.classList.remove("colored");
-            }
-        })
+        clone.addEventListener('mouseover', pushColor);
 
         main.appendChild(clone)
     }
 }
+function pushColor(e){
+    if(!grayMode){
+        if (!(e.target.classList.contains("colored")) && drawMode && !erasor){
+            e.target.style['background-color'] = getColor();
+            e.target.classList.add("colored");
+            e.target.classList.add("written");
+        }
+        else if(erasor && drawMode){
+            e.target.style['background-color'] = "#fff";
+            e.target.classList.remove("colored");
+        }
+    }
+    else{
+        if(drawMode && !erasor){
+            let scale = parseInt(e.target.dataset.grayscale) + 1;
+            if(scale - 1 == 0){
+                e.target.classList.add("colored");
+            }
+            if(!e.target.classList.contains("written") && scale < 11){;
+                e.target.style['background-color'] = `hsl(0, 0%, ${100 - (scale * 10)}%)`;
+                e.target.dataset.grayscale = scale;
+            }
+        }
+    }
+}
+
 function resetFreeVars(){
     erasor = 0;
     rainbow = 0;
+    grayMode = 0;
 }
-function reset(){
-    resetFreeVars()
+function reset(def = 1){
+    resetFreeVars();
     let gridLst = document.querySelectorAll(".grid-ele")
-    gridLst.forEach((gl) => {gl.remove()})
-    getBoxSz();
-    pushBoxes()
+    gridLst.forEach((gl) => {gl.remove()});
+    if(def){
+        getBoxSz();
+    }
+    pushBoxes();
 }
 
 function erase(){
@@ -82,6 +102,10 @@ function erase(){
 function rainbowMode(){
     resetFreeVars()
     rainbow = 1;
+}
+function grayScale(){
+    resetFreeVars();
+    grayMode = 1;
 }
 
 function main(){
