07f9a750b0e45d1f2ff6feb98ccd6cdc82e2a49c
Parth099 <parthnpatel715@gmail.cm>
2021-06-14 20:27:00 -0400

    added more draw options

--- js/src.js
+++ js/src.js
@@ -1,8 +1,16 @@
 boxWdth = 700
-numBox = 16
+numBox = 16;
+erasor = 0;
+rainbow = 0;
+colorMain = "#000";
 
-function getBoxSz(){
-    numBox = prompt("Enter a box size, max: 100", 20);
+function getcolor(){
+    return (rainbow) ? "#"+Math.floor(Math.random()*16777215).toString(16): colorMain;
+}
+function getBoxSz(flag = 1){
+    if(flag = 1){
+        numBox = prompt("Enter a box size, max: 100", 20);
+    }
     while(numBox < 0 || numBox > 100){
         numBox = prompt("Enter a box size, MAX: 100", 20);
         console.log(numBox)
@@ -30,21 +38,41 @@ function pushBoxes(){
         clone.style['background-color'] = 'white'
 
         //listener
-        clone.addEventListener('mouseover', function(e){
-            e.target.style['background-color'] = "#000";
-            e.target.classList.toggle("colored");
-        }, {once: true})
+        clone.addEventListener('mousedown', function(e){
+            if (!(e.target.classList.contains("colored"))){
+                e.target.style['background-color'] = getcolor()
+                e.target.classList.add("colored");
+            }
+            if(erasor){
+                e.target.style['background-color'] = "#fff"
+                e.target.classList.remove("colored");
+            }
+        })
 
         main.appendChild(clone)
     }
 }
 function reset(){
-
-    console.log("called")
+    erasor = 0;
+    rainbow = 0;
     let gridLst = document.querySelectorAll(".grid-ele")
     gridLst.forEach((gl) => {gl.remove()})
     getBoxSz();
     pushBoxes()
 }
+
+function erase(){
+    erasor = 1;
+    rainbow = 0;
+}
+function rainbowMode(){
+    erasor = 0
+    rainbow = 1;
+}
+var cpp = document.getElementById("cpp")
+cpp.addEventListener("change", function(e){
+    colorMain = e.target.value;
+    rainbow = 0;
+});
 
 pushBoxes()
